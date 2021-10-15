c8fb5fca29b65b8d6767100b002327e5db1f044e
Mitchell Barton <mitchell@welikesmall.com>
2021-10-15 13:05:02 -0600

    changing target to static

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ const title = 'Zak Klauck'
 const description = 'Zak Klauck'
 
 export default {
-  target: 'server',
+  target: 'static',
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
